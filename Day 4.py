3a1619585a7524c0d37d28ecba765503db4d26c4
rhiannonmay <rhiannonmay@Edgar.local>
2021-11-12 02:01:50 -0700

    Day 4 edits (part 1), not complete.

--- Day 4.py
+++ Day 4.py
@@ -1,35 +1,40 @@
 #part 1
 
 with open('Input 4.txt') as fp:
-    lines = [f.strip() for f in fp]
+    data = [f.strip() for f in fp]
 
-curr_passport = {}
-all_passports = []
-valid_passports = []
-req_keys =[
-    "byr", # (Birth Year)
-    "iry", # (Issue Year)
-    "eyr", # (Expiration Year)
-    "hgt", # (Height)
-    "hcl", # (Hair Color)
-    "ecl", # (Eye Color)
-    "pid", # (Passport ID)
-]
+def part1 (data):
+    curr_passport = {}
+    all_passports = []
+    valid_passport_count = 0
+    req_fields =[
+        "byr", # (Birth Year)
+        "iry", # (Issue Year)
+        "eyr", # (Expiration Year)
+        "hgt", # (Height)
+        "hcl", # (Hair Color)
+        "ecl", # (Eye Color)
+        "pid", # (Passport ID)
+    ]
+    key_count = 0
+    current = 0 
 
-key_count = 0
+    for line in data:
+        if line == " ":
+            if current == len(req_fields):
+                valid_passport_count += 1
 
-for line in lines:
-    if line != "":
-        temp_line = line.split(" ")
-        temp_dict = {item.split(":")[0]:item.split(":")[1] for item in temp_line}
-        for key, value in temp_dict.items():
-            curr_passport[key] = value
-    else:
-        if all(field in curr_passport for field in req_keys):
-            valid_passports.append(curr_passport)
-        curr_passport = {}
-        all_passports.append(curr_passport)
-all_passports.append(curr_passport)
+    for field in line.split():
+            key, value = field.split(":")
+            if key in req_fields:
+                current += 1
+    return valid_passport_count
+    
+print("Solution:  ", part1(data))
 
-len(all_passports), len(valid_passports)
 
+#        if line == " ":
+#            valid_passport_count.append(curr_passport)
+#        curr_passport = {}
+#        all_passports.append(curr_passport)
+#all_passports.append(curr_passport)
